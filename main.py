f3931bbeb4f05ef73c35ce04978a131ede09e393
Crackfever <b1s2p3@gmail.com>
2020-01-27 20:16:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Welcome to the Temperature Conversion App")
+
+#get input
+temp_f = float(input("\nWhat is the given temperature in degrees Fahrenheit: "))
+
+#Covert temps
+temp_c = (5/9)*(temp_f - 32)
+temp_k = temp_c + 273.15
+
+#round temps
+temp_f = round(temp_f, 4)
+temp_c = round(temp_c, 4)
+temp_k = round(temp_k, 4)
+
+#summary table
+print("\nDegrees Fahrenheit:\t" + str(temp_f))
+print("Degrees Celsius:\t" + str(temp_c))
+print("Degrees Kelvin:\t\t" + str(temp_k))
